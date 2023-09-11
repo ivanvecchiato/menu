1f5f2fab23f259b00d1afbef3e6cda73b7acf5bc
ivanvecchiato <ivan.vecchiato@gmail.com>
2023-09-11 17:04:14 +0200

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/report">Report</router-link>
   </nav>
   <router-view/>
 </template>
@@ -13,10 +13,36 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  min-height: 100vh;
+  background: #F5F9FF;  
+}
+
+:root {
+  --primary-color-500: #0BA5EC;
+  --primary-color-700: #0086c9;
+
+  --secondary-color-300: #AFB5D9;
+  --secondary-color-400: #717bbc;
+
+  --input-color: #99A3BA;
+  --input-border: #CDD9ED;
+  --input-background: #fff;
+  --input-placeholder: #CBD1DC;
+  
+  --input-border-focus: #275EFE;
+  
+  --group-color: var(--input-color);
+  --group-border: var(--input-border);
+  --group-background: #EEF4FF;
+  
+  --group-color-focus: #fff;
+  --group-border-focus: var(--input-border-focus);
+  --group-background-focus: #678EFE;
 }
 
 nav {
-  padding: 30px;
+  padding: 0px;
 }
 
 nav a {
@@ -27,4 +53,44 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
+input[type=text] {
+  display: block;
+  padding: 4px 8px;
+  font-size: 14px;
+  font-weight: 500;
+  font-family: inherit;
+  border-radius: 6px;
+  -webkit-appearance: none;
+  color: var(--input-color);
+  border: 1px solid var(--input-border);
+  background: var(--input-background);
+  transition: border .3s ease;
+  &::placeholder {
+      color: var(--input-placeholder);
+  }
+  &:focus {
+      outline: none;
+      border-color: var(--input-border-focus);
+  }
+}
+
+button {
+  background-color: var(--primary-color-500);
+  border: none;
+  color: white;
+  padding: 8px 12px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 2px 10px;
+  border-radius: 4px;
+  transition-duration: 0.4s;
+}
+
+button:hover {
+  background-color: var(--primary-color-700);
+  color: white;
+}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,197 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="report">
+    <div class="container">
+      <div class="header">PRANZO</div>
+      <div class="form-group">
+        <div v-for="item in items.pranzo.primi" :key="item.id" class="form-row">
+          <input type="text" size="12" v-model="item.name"/>
+          <input type="text" size="1" v-model="item.quantity.regular"/>
+          <input type="text" size="1" v-model="item.quantity.small"/>
+        </div>
+      </div>
+      <div class="form-group">
+        <div v-for="item in items.pranzo.secondi" :key="item.id" class="form-row">
+          <input type="text" size="12" v-model="item.name"/>
+          <input type="text" size="1" v-model="item.quantity.regular"/>
+          <input type="text" size="1" v-model="item.quantity.small"/>
+        </div>
+      </div>
+      <div class="header">CENA</div>
+      <div class="form-group">
+        <div v-for="item in items.cena.primi" :key="item.id" class="form-row">
+          <input type="text" size="12" v-model="item.name"/>
+          <input type="text" size="1" v-model="item.quantity.regular"/>
+          <input type="text" size="1" v-model="item.quantity.small"/>
+        </div>
+      </div>
+      <div class="form-group">
+        <div v-for="item in items.cena.secondi" :key="item.id" class="form-row">
+          <input type="text" size="12" v-model="item.name"/>
+          <input type="text" size="1" v-model="item.quantity.regular"/>
+          <input type="text" size="1" v-model="item.quantity.small"/>
+        </div>
+      </div>
+    </div>
+    <div class="buttons">
+      <button>prev</button>
+      <button>next</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      items: {
+        pranzo: {
+          primi: [],
+          secondi: []
+        },
+        cena: {
+          primi: [],
+          secondi: []
+        }
+      },
+      num_of_items: 8
+    }
+  },
+  methods: {
+    composeItems: function() {
+      for(var i=0; i<this.num_of_items; i++) {
+        var item = {
+          id: 'pp' + (i+1),
+          name: 'item ' + (i+1),
+          quantity: {
+            regular: 0,
+            small: 0
+          }
+        }
+
+        this.items.pranzo.primi.push(item);
+      }
+      for(i=0; i<this.num_of_items; i++) {
+        item = {
+          id: 'ps' + (i+1),
+          name: 'item ' + (i+1),
+          quantity: {
+            regular: 0,
+            small: 0
+          }
+        }
+
+        this.items.pranzo.secondi.push(item);
+      }
+      for(i=0; i<this.num_of_items; i++) {
+        item = {
+          id: 'cp' + (i+1),
+          name: 'item ' + (i+1),
+          quantity: {
+            regular: 0,
+            small: 0
+          }
+        }
+
+        this.items.cena.primi.push(item);
+      }
+      for(i=0; i<this.num_of_items; i++) {
+        item = {
+          id: 'cs' + (i+1),
+          name: 'item ' + (i+1),
+          quantity: {
+            regular: 0,
+            small: 0
+          }
+        }
+
+        this.items.cena.secondi.push(item);
+      }
+    }
+  },
+  mounted() {
+    this.composeItems();
+    console.log(this.items)
   }
+
+
 }
 </script>
+
+<style scoped>
+.report {
+  text-align: center;
+}
+.container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 8px;
+}
+.form-group {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+  & > span,
+  .form-field {
+      white-space: nowrap;
+      display: block;
+      &:not(:first-child):not(:last-child) {
+          border-radius: 0;
+      }
+      &:first-child {
+          border-radius: 6px 0 0 6px;
+      }
+      &:last-child {
+          border-radius: 0 6px 6px 0;
+      }
+      &:not(:first-child) {
+          margin-left: -1px;
+      }
+  }
+.form-row {
+  display: flex;
+  gap: 4px;
+}
+  .form-field {
+      position: relative;
+      z-index: 1;
+      flex: 1 1 auto;
+      width: 1%;
+      margin-top: 0;
+      margin-bottom: 0;
+  }
+  & > span {
+      text-align: center;
+      padding: 8px 12px;
+      font-size: 14px;
+      line-height: 25px;
+      color: var(--group-color);
+      background: var(--group-background);
+      border: 1px solid var(--group-border);
+      transition: background .3s ease, border .3s ease, color .3s ease;
+  }
+  &:focus-within {
+      & > span {
+          color: var(--group-color-focus);
+          background: var(--group-background-focus);
+          border-color: var(--group-border-focus);
+      }
+  }
+}
+.header {
+  grid-column: span 2;
+  font-size: 1.2em;
+  font-weight: bold;
+  margin: 10px;
+}
+.buttons {
+  margin: 10px;
+}
+</style>
